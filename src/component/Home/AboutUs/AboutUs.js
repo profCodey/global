1487001d5126df7e935bac8c0f8adbc47e0b91e4
profCodey <otunaiyademilade@gmail.com>
2023-09-01 00:38:06 +0100

    added helmet SEO libary

--- src/component/Home/AboutUs/AboutUs.js
+++ src/component/Home/AboutUs/AboutUs.js
@@ -9,20 +9,16 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import security from "../../../assets/images/security.jpg";
 
-
 function AboutUs() {
-let iconOne = <FontAwesomeIcon icon={faPersonMilitaryPointing} />;
-    let iconTwo = <FontAwesomeIcon icon={faBuildingShield} />;
-    let iconThree = <FontAwesomeIcon icon={faHandshakeAngle} />;
-    let text1 =
-      "Our door supervisors are vetted to the British Standard BS7858. They are registered and trained to SITO and BIIAB standards (National Certificate for Door Supervisors Licensed Premises). They are handpicked and undergo rigorous training, equipped with the latest security techniques and crowd management strategies.";
-      let text2 =
-          "We are licensed by the Security Industry Authority (SIA) and other security parastatals and we adhere strictly to all industry regulations and guidelines . You can trust us to conduct our services responsibly and with the utmost professionalism.";
-    let text3 =
-      "We prioritize the safety and satisfaction of your customers and staff. Our door supervisors are not just vigilant security personnel; they also embody a friendly and approachable demeanor, making your patrons feel comfortable and welcomed.";
-
-
-    
+  let iconOne = <FontAwesomeIcon icon={faPersonMilitaryPointing} />;
+  let iconTwo = <FontAwesomeIcon icon={faBuildingShield} />;
+  let iconThree = <FontAwesomeIcon icon={faHandshakeAngle} />;
+  let text1 =
+    "Our door supervisors are vetted to the British Standard BS7858. They are registered and trained to SITO and BIIAB standards (National Certificate for Door Supervisors Licensed Premises). They are handpicked and undergo rigorous training, equipped with the latest security techniques and crowd management strategies.";
+  let text2 =
+    "We are licensed by the Security Industry Authority (SIA) and other security parastatals and we adhere strictly to all industry regulations and guidelines . You can trust us to conduct our services responsibly and with the utmost professionalism.";
+  let text3 =
+    "We prioritize the safety and satisfaction of your customers and staff. Our door supervisors are not just vigilant security personnel; they also embody a friendly and approachable demeanor, making your patrons feel comfortable and welcomed.";
 
   return (
     <>
@@ -33,11 +29,11 @@ let iconOne = <FontAwesomeIcon icon={faPersonMilitaryPointing} />;
             <span style={{ color: "#ff9d2f" }}> 10 Years</span> Of Experience
           </h2>
           <p>
-            We are your trusted partner in providing professional door
-            supervisory services across the UK. We take pride in ensuring the
-            safety and security of your establishment and its patrons, allowing
-            you to focus on providing an enjoyable and welcoming environment for
-            all.
+            We provide top event security services in the UK- Get security for nightclub, bar, restaurant and any special event, CCTV securty. We are your
+            trusted partner in providing professional door supervisory services
+            across the UK. We take pride in ensuring the safety and security of
+            your establishment and its patrons, allowing you to focus on
+            providing an enjoyable and welcoming environment for all.
           </p>
           <div className="aboutUsTopChile">
             <Value title="Trained Professionals" text={text1} icon={iconOne} />
