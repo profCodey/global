2744ef24ce6936cc9244a3d33bae1b7a23201b58
profCodey <otunaiyademilade@gmail.com>
2023-09-03 17:39:44 +0100

    retail security started

--- src/component/AboutPage/Services.js
+++ src/component/AboutPage/Services.js
@@ -10,27 +10,27 @@ import consultancy from "../../assets/images/consultancy.jpg";
 function OurServices() {
   let header1 = "Access Control Management";
   let text1 =
-    "Efficiently manage entry and exit points, check security passes, ensuring only authorized personnel are allowed.";
+    "We Efficiently manage entry and exit points, check security passes, ensuring only authorized personnel are allowed.";
   
-   let header2 = "Crowd Management";
+   let header2 = "Retail Security Services";
   let text2 =
-    "Handle crowd flow, prevent overcrowding, and ensure a smooth experience.";
+    "We ensure to keep your shoppers and staff safe and to reduce the risk of theft. In case of emergencies, our door supervisors are prepared to respond swiftly, prioritizing the safety of everyone in your retail space. ";
   
-   let header3 = "Event Security";
+   let header3 = "Event Security & Crowd Management";
   let text3 =
-    "Secure events of all sizes, ensuring safety and order during gatherings.";
+    "We Secure events of all sizes, ensuring safety and order during gatherings.";
   
    let header4 = "CCTV Monitoring";
   let text4 =
-    "Offer remote monitoring of surveillance cameras to enhance security.";
+    "We Offer remote monitoring of surveillance cameras to enhance security.";
   
    let header5 = "Fire Checks & Safety Audits";
   let text5 =
-    "Evaluate venues for health and safety compliance and recommend improvements, conduct routine fire safety inspections and drills.";
+    "We Evaluate venues for health and safety compliance and recommend improvements, conduct routine fire safety inspections and drills.";
   
    let header6 = "Consulting Services:";
   let text6 =
-    "Provide expert advice on security measures, risk assessments, and protocol development.";
+    "We Provide expert advice on security measures, risk assessments, and protocol development.";
   
   
   
