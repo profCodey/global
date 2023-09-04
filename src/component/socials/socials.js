56874fa453ba6c9dd2b7298dfd2f5a6f064e7efe
profCodey <otunaiyademilade@gmail.com>
2023-09-04 14:10:52 +0100

    remove twitter logo

--- src/component/socials/socials.js
+++ src/component/socials/socials.js
@@ -23,7 +23,7 @@ function Socials({
   return (
     <div style={socialIcon}>
       <FontAwesomeIcon icon={faSquareFacebook} style={socialIcon} />
-      <FontAwesomeIcon icon={faSquareXTwitter} style={socialIcon} />
+      {/* <FontAwesomeIcon icon={faSquareXTwitter} style={socialIcon} /> */}
       <FontAwesomeIcon icon={faSquareInstagram} style={socialIcon} />
       <span className="ml-0 -mt-2 md: lg:-ml-3 lg:-mt-2">
         <WhatsAppButton style={{}} fontSize={fontSize} />
